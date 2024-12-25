b44b07a2e25bfb473ed78e3b30888392b9eff81f
Mārtiņš <martis@localhost.localdomain>
2024-12-25 17:15:17 +0200

    Updated templating

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 async function fillProducts(products){
-    const template = document.querySelector("div.products-grid div.template")
-    products.forEach((e) => {
-        console.log(e);
+    const template_h = document.querySelector("div.products-grid div.template")
+    products.forEach((product) => {
+        template = template_h.cloneNode(true)
+        template.querySelector("h3.name").textContent = product.title;
+        template.querySelector("p.price").textContent = "€" + product.price;
+        template.querySelector("img").setAttribute("src", product.image_url);
+        template.querySelector("img").setAttribute("alt", product.alt);
+        template.querySelector("button.primary").setAttribute("product_id", product.id);
+        template.classList.remove("template");
+        template.removeAttribute("hidden");
+        template_h.parentElement.appendChild(template);
+        console.log(product);
     });
 }
 async function applyTranslations(languageFile) {
@@ -18,6 +27,7 @@ async function applyTranslations(languageFile) {
             }
         });
         fillProducts(translations["products"]);
+        return translations["products"];
     } catch (error) {
         console.error("Error loading translations:", error);
     }
